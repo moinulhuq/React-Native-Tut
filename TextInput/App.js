ee16528e42fd9c642426e4bcd35fd02e84bcf8f6
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2019-05-14 14:46:42 +1200

    Create App.js

---
+++ TextInput/App.js
@@ -0,0 +1,91 @@
+/*
+1. 'TextInput' enter text (single line).
+
+example
+-------*/
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {Uname: ''};
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput 
+        	underlineColorAndroid = "transparent"
+        	placeholder = "User name"
+        	placeholderTextColor = "gray"
+        	autoCapitalize = "none"
+			style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+			onChangeText={(Uname) => this.setState({Uname})}
+			value={this.state.Uname}
+        />
+        <Text>{this.state.Uname}</Text>
+      </View>
+    );
+  }
+}
+
+/*
+2. 'TextInput' enter text (multiple line).
+
+example
+-------*/
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {Uname: ''};
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput 
+        	underlineColorAndroid = "transparent"
+        	placeholder = "User name"
+        	placeholderTextColor = "gray"
+        	autoCapitalize = "none"
+			style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+			onChangeText={(Uname) => this.setState({Uname})}
+			value={this.state.Uname}
+			multiline={true}
+        />
+        <Text>{this.state.Uname}</Text>
+      </View>
+    );
+  }
+}
+
+/*
+3. 'TextInput' enter numeric (multiple line).
+
+example
+-------*/
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {Uname: ''};
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput 
+        	underlineColorAndroid = "transparent"
+        	placeholder = "User name"
+        	placeholderTextColor = "gray"
+        	autoCapitalize = "none"
+			style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+			onChangeText={(Uname) => this.setState({Uname})}
+			value={this.state.Uname}
+			keyboardType="numeric"
+        />
+        <Text>{this.state.Uname}</Text>
+      </View>
+    );
+  }
+}
+
+
+
